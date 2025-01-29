3d1df3933361334c45f967adbe67114a8d882781
Ikenna Umeh <kennaumeh@gmail.com>
2025-01-29 14:11:00 +0100

    Finished the info controller

---
+++ BasicInfo.API/Data/DTO/InfoResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicInfo.API.Data.DTO
+{
+    public class InfoResponseDto
+    {
+        public string email { get; set; }
+        public string date_time { get; set; }
+        public string github_url { get; set; }
+    }
+}
