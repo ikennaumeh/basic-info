e0d92f09067cee6ccccf06f4b2918861fbaebbae
Ikenna Umeh <kennaumeh@gmail.com>
2025-01-29 20:44:27 +0100

    Added dockerfile

--- BasicInfo.API/obj/Debug/net8.0/BasicInfo.API.AssemblyInfo.cs
+++ BasicInfo.API/obj/Debug/net8.0/BasicInfo.API.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("8f9ab5a7-d3b2-4ca9-974e-cfb01c1eaa6e")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8ec6532a64c83918adabab1e13439abdc177a85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c86d1ab6da83e929edab2fd16b773dc23c8bcbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
