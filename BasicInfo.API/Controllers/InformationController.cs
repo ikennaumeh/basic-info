5c0e14ae161c7be2bc0417103ade071a2e6b07da
Ikenna Umeh <kennaumeh@gmail.com>
2025-01-29 14:59:46 +0100

    Edited InformationController

---
+++ BasicInfo.API/Controllers/InformationController.cs
@@ -0,0 +1,24 @@
+﻿using BasicInfo.API.Data.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicInfo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InformationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            var info = new InfoResponseDto
+            {
+                email = "kennaumeh@gmail.com",
+                date_time = DateTime.UtcNow.ToString("o"),
+                github_url = "https://github.com/ikennaumeh/basic-info"
+            };
+
+            return Ok(info);
+        }
+    }
+}
